eb7929a43362b818aebfb0f3a2124fabfbcadcb2
jorge perez <jorgeperez00@hotmail.com>
2020-07-23 18:30:42 -0400

    added default

--- CSharpExamplesNetCore/MathExpressionExample.cs
+++ CSharpExamplesNetCore/MathExpressionExample.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 
 namespace CSharpExamples {
     public partial class MathExpressionExample : Form {
@@ -33,6 +34,7 @@ namespace CSharpExamples {
             snippets.Add("var c = 1;");
             snippets.Add("var c = \"testing\";");
             snippets.Add("var c = (true) ? (false) ? 1 : 2 : 3;");
+            snippets.Add("var c = (true == true) ? (true == false) ? 1 : 2 : 3;");
         }
 
         private void btnSubmit_Click(object sender, EventArgs e)         {
@@ -41,6 +43,7 @@ namespace CSharpExamples {
         }
         private void btnRun_Click(object sender, EventArgs e)
         {
+            
             ScriptState state = CSharpScript.RunAsync(txtScript.Text).Result;
             var value = state.GetVariable("c");
             txtResult.Text = value.Value.ToString();
